3ea81524147de01ebf1f0309638c022dcae773af
Niti2023 <161957376+Niti2023@users.noreply.github.com>
2024-06-25 22:58:45 +0530

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,50 @@
+
+import java.util.Scanner;    // This is a scanner class used for Taking input from user...
+
+public class Operators {
+
+    public static void main(String_in[] args) {
+        
+        System.out.println("Operatoe in JAVA & Getting user input..");
+
+        //   Arithematic Operator...  ( + , - , / , * , %)
+
+        // int a=10;
+        // int b=3;
+
+        // System.out.println("Addition of two number : "+ (a+b));
+        // System.out.println("Substract of two number : "+ (a-b));
+        // System.out.println("Division of two number : "+ (a/b));
+        // System.out.println("Multiplication of two number : "+ (a*b));
+        // System.out.println("Modulus of two number : "+ (a%b));
+        // System.out.println("relational operator : "+ (a==b));
+
+        // boolean x = ((a>b) && (b<a));
+        // System.out.println("logical operator : "+ x);
+
+
+
+        // Scanner s = new Scanner(System.in);     // Here 's' is a object of scanner class...
+        // System.out.println("Enter first number : ");
+        // int a = s.nextInt(); 
+        
+        // System.out.println("Enter second number : ");
+        // int b = s.nextInt();
+
+        // int sum = a+b;
+        // System.out.println("Sum of two number : "+sum);
+
+
+        String_in a = "Abhishek";
+        System.out.print("My name is : "+a);
+        
+
+        int b = 7;
+        float c = 7.45f;
+        System.out.printf("Value of b is %d and value of c is %f.",b,c);
+        System.out.format("Value of b is %d and value of c is %f.",b,c);
+
+
+    }
+    
+}
